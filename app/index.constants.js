9007db16ff84faf99020db97b2e0954adb3f77ff
Harizo <rajaonarisoazo@gmail.com>
2019-08-05 16:32:40 +0300

    ajout type formation recue dans enquete_individu

--- app/index.constants.js
+++ app/index.constants.js
@@ -13,7 +13,7 @@
 		.constant("apiUrl_serve_central", "http://192.168.88.200/2019/comores/api/index.php/api/")//ip serveur cantrale si dans les ile pour l'envoi des données
 
 		.constant("apiUrlrecommandation", "recommandation/")
-		.constant("serveur_central", false);
+		.constant("serveur_central", true);
 
 		/*.constant("apiUrl", "http://serverdnsps:81/2019/comores/api/index.php/api/")
 		.constant("apiUrl_serve_central", "http://serverdnsps:81/2019/comores/api/index.php/api/")//ip serveur cantrale si dans les ile pour l'envoi des données
